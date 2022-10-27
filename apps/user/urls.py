8173d6bf92bd37dba389afb734518005e0262293
WayupKG <adikgk232323@gmail.com>
2022-10-27 22:33:34 +0600

    add urlpatterns name, fix jwt_view > jwt_views

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path, include
 
-from rest_framework_simplejwt import views as jwt_view
+from rest_framework_simplejwt import views as jwt_views
 
 from apps.user import views
 
 
 urlpatterns = [
-    path('token/', jwt_view.TokenObtainPairView.as_view()),
-    path('token/refresh/', jwt_view.TokenRefreshView.as_view()),
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name="login"),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name="login_refresh"),
 
-    path('sign-up/', views.RegistrationAPIView.as_view()),
-    path('extra-info-mentor/', views.ExtraInfoMentorAPIView.as_view()),
+    path('sign-up/', views.RegistrationAPIView.as_view(), name="sign-up"),
+    path('extra-info-mentor/', views.ExtraInfoMentorAPIView.as_view(), name="extra-info-mentor"),
 
-    path('reset-password/', views.EmailPasswordResetAPIView.as_view()),
+    path('reset-password/', views.EmailPasswordResetAPIView.as_view(), name="reset-password"),
     path('reset-password-confirm/<uidb64>/<token>/',
          views.PasswordResetConfirmAPIView.as_view(), name="reset-password-confirm"),
 ]
