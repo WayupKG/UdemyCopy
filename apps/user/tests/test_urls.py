40bec86254c7a1957f282ff9948d867dfa037d26
WayupKG <adikgk232323@gmail.com>
2022-10-27 22:33:54 +0600

    add test urls

---
+++ apps/user/tests/test_urls.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from django.urls import resolve, reverse
+
+from rest_framework_simplejwt import views as jwt_views
+
+from apps.user import views
+
+
+class TestUrls(TestCase):
+    """Тестирование url адресы"""
+
+    def test_login(self):
+        url = reverse('login')
+        view_class = resolve(url).func.view_class
+        return self.assertEqual(view_class, jwt_views.TokenObtainPairView)
+
+    def test_login_refresh(self):
+        url = reverse('login_refresh')
+        view_class = resolve(url).func.view_class
+        return self.assertEqual(view_class, jwt_views.TokenRefreshView)
+
+    def test_sign_up(self):
+        url = reverse('sign-up')
+        view_class = resolve(url).func.view_class
+        return self.assertEqual(view_class, views.RegistrationAPIView)
+
+    def test_extra_info_mentor(self):
+        url = reverse('extra-info-mentor')
+        view_class = resolve(url).func.view_class
+        return self.assertEqual(view_class, views.ExtraInfoMentorAPIView)
+
+    def test_reset_password(self):
+        url = reverse('reset-password')
+        view_class = resolve(url).func.view_class
+        return self.assertEqual(view_class, views.EmailPasswordResetAPIView)
+
+    def test_reset_password_confirm(self):
+        url = reverse('reset-password-confirm', args=["Mg", "bdtsfv-e732384f23caf82fa2cd2251f4e59105"])
+        view_class = resolve(url).func.view_class
+        return self.assertEqual(view_class, views.PasswordResetConfirmAPIView)
